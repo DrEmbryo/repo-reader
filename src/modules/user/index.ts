dd8ab18c4d335cd96fd25e7db73d73efdc77527e
Pavel Goroshko <mcredpower@gmail.com>
2024-02-01 23:26:42 +0100

    add some content to user page

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -9,11 +9,24 @@ userRouts.get("/", async function (req, res) {
 });
 
 userRouts.post("/search", async (req, res) => {
-  const user = await octokitClient.request("GET /users/{username}", {
-    username: req.body.username,
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-  });
-  console.log(user);
+  try {
+    const user = await octokitClient.request("GET /users/{username}", {
+      username: req.body.username,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+
+    const following = await octokitClient.request(
+      `GET ${user.data.following_url}`
+    );
+
+    return res.render("./user/components/profile-grid", {
+      user: user.data,
+      following: following.data,
+    });
+  } catch (requestError: any) {
+    if (requestError.status === 404)
+      return res.render("./user/components/empty-results");
+  }
 });
